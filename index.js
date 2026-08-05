502511c85791f49b0bb8c54b6a5955b964056ddb
upi1682 <upi538287@gmail.com>
2026-08-05 14:42:32 +0700

    chore: initialize express app and set port in index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const connectToDatabase = require('./config/db');
 
-
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 
 
